af81438bdcaab9b0f7bd4069d5d1266e973ac16c
eezstreet <eezstreet@live.com>
2013-08-08 17:23:46 -0400

    /win32/ merge

--- codemp/win32/resource.h
+++ codemp/win32/resource.h
@@ -1,10 +1,29 @@
+//       ____ ___________________   ___           ____  __ _______   ___  ________  ___ ______________
+//      |    |\_   _____/\______ \ |   |         |    |/ _|\      \ |   |/  _____/ /   |   \__    ___/
+//      |    | |    __)_  |    |  \|   |         |      <  /   |   \|   /   \  ___/    ~    \|    |   
+//  /\__|    | |        \ |    `   \   |         |    |  \/    |    \   \    \_\  \    Y    /|    |   
+//  \________|/_______  //_______  /___|         |____|__ \____|__  /___|\______  /\___|_  / |____|   
+//                    \/         \/                      \/       \/            \/       \/           
+//                         ________    _____   ____       _____  ____  ___ ______________ _________   
+//                        /  _____/   /  _  \ |    |     /  _  \ \   \/  /|   \_   _____//   _____/   
+//                       /   \  ___  /  /_\  \|    |    /  /_\  \ \     / |   ||    __)_ \_____  \    
+//                       \    \_\  \/    |    \    |___/    |    \/     \ |   ||        \/        \   
+//                        \______  /\____|__  /_______ \____|__  /___/\  \|___/_______  /_______  /   
+//                               \/         \/        \/	   \/	   \_/			  \/        \/ (c)
+// resource.h
+
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by winquake.rc
+// Microsoft Visual C++ generated include file.
+// Used by JK2cgame.rc
 //
+#define VERSION_MINOR_RELEASE           0
+#define VERSION_INTERNAL_BUILD          0
 #define IDS_STRING1                     1
 #define IDI_ICON1                       1
 #define IDB_BITMAP1                     1
+#define VERSION_MAJOR_RELEASE           1
+#define VERSION_EXTERNAL_BUILD          1
+#define VERSION_BUILD_NUMBER            91
 #define IDB_BITMAP2                     128
 #define IDC_CURSOR1                     129
 #define IDC_CURSOR2                     130
