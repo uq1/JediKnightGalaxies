uq1/JediKnightGalaxies

commit c92207fb1f3d9c56231c71d0853e4609138abe1f

First round of safety/sanity/BOF checks

codemp/qcommon/stringed_ingame.cpp
@@ -167,7 +167,7 @@ char *CStringEdPackage::Filename_PathOnly(const char *psFilename)
 {
 	static char sString[ iSE_MAX_FILENAME_LENGTH ];
 
-	strcpy(sString,psFilename);	
+	Q_strncpyz(sString, psFilename, sizeof(sString));
 		
 	char *p1= strrchr(sString,'\\');
 	char *p2= strrchr(sString,'/');
@@ -189,7 +189,7 @@ char *CStringEdPackage::Filename_WithoutExt(const char *psFilename)
 {
 	static char sString[ iSE_MAX_FILENAME_LENGTH ];
 
-	strcpy(sString,psFilename);
+	Q_strncpyz(sString, psFilename, sizeof(sString));
 
 	char *p = strrchr(sString,'.');		
 	char *p2= strrchr(sString,'\\');
@@ -623,7 +623,7 @@ const char *CStringEdPackage::ParseLine( const char *psLine )
 			{
 				static const char sSeperators[] = " \t";
 				char sFlags[1024]={0};	// 1024 chars should be enough to store 8 flag names
-				strncpy(sFlags, psLine, sizeof(sFlags)-1);
+				Q_strncpyz(sFlags, psLine, sizeof(sFlags));
 				char *psToken = strtok( sFlags, sSeperators );
 				while( psToken != NULL )
 				{
@@ -672,7 +672,9 @@ const char *CStringEdPackage::ParseLine( const char *psLine )
 				{
 					iCharsToCopy = sizeof(sThisLanguage)-1;
 				}
-				strncpy(sThisLanguage, psLine, iCharsToCopy);	// already declared as {0} so no need to zero-cap dest buffer
+
+//				strncpy(sThisLanguage, psLine, iCharsToCopy);	// already declared as {0} so no need to zero-cap dest buffer
+				Q_strncpyz(sThisLanguage, psLine, iCharsToCopy);	// yea, whatever
 
 				psLine += strlen(sThisLanguage);
 				const char *_psSentence = ConvertCRLiterals_Read( InsideQuotes( psLine ) );

codemp/RMG/RM_Instance.h
@@ -66,8 +66,8 @@ public:
 	virtual void		Preview				( const vec3_t from );
 
 	virtual void		SetArea				( CRMAreaManager* amanager, CRMArea* area ) { mArea = area; }
-	virtual void		SetFilter			( const char *filter ) { strcpy(mFilter, filter); }
-	virtual void		SetTeamFilter		( const char *teamFilter ) { strcpy(mTeamFilter, teamFilter); }
+	virtual void		SetFilter			( const char *filter ) { Q_strncpyz(mFilter, filter, sizeof(mFilter));  }
+	virtual void		SetTeamFilter		( const char *teamFilter ) { Q_strncpyz(mTeamFilter, teamFilter, sizeof(mTeamFilter)); }
 	void				SetObjective		( CRMObjective* obj ) { mObjective = obj; }
 	CRMObjective*		GetObjective		(void) {return mObjective;}
 	bool				HasObjective		() {return mObjective != NULL;}

codemp/botlib/be_aas_main.cpp
@@ -206,7 +206,7 @@ int AAS_LoadFiles(const char *mapname)
 	char aasfile[MAX_PATH];
 //	char bspfile[MAX_PATH];
 
-	strcpy(aasworld.mapname, mapname);
+	Q_strncpyz(aasworld.mapname, mapname, sizeof(aasworld.mapname));
 	//NOTE: first reset the entity links into the AAS areas and BSP leaves
 	// the AAS link heap and BSP link heap are reset after respectively the
 	// AAS file and BSP file are loaded

codemp/botlib/be_ai_chat.cpp
@@ -1008,21 +1008,21 @@ bot_randomlist_t *BotLoadRandomStrings(char *filename)
 			} //end if
 			while(!PC_CheckTokenString(source, "}"))
 			{
-				size_t len;
+				size_t len2;
 				if (!BotLoadChatMessage(source, chatmessagestring))
 				{
 					FreeSource(source);
 					return NULL;
 				} //end if
-				len = strlen(chatmessagestring) + 1;
-				len = PAD(len, sizeof(long));
-				size += sizeof(bot_randomstring_t) + len;
+				len2 = strlen(chatmessagestring) + 1;
+				len2 = PAD(len, sizeof(long));
+				size += sizeof(bot_randomstring_t) + len2;
 				if (pass)
 				{
 					randomstring = (bot_randomstring_t *) ptr;
 					ptr += sizeof(bot_randomstring_t);
 					randomstring->string = ptr;
-					ptr += len;
+					ptr += len2;
 					strcpy(randomstring->string, chatmessagestring);
 					//
 					random->numstrings++;
@@ -1440,7 +1440,7 @@ int BotFindMatch(char *str, bot_match_t *match, unsigned long int context)
 	int i;
 	bot_matchtemplate_t *ms;
 
-	strncpy(match->string, str, MAX_MESSAGE_SIZE);
+	Q_strncpyz(match->string, str, MAX_MESSAGE_SIZE);
 	//remove any trailing enters
 	while(strlen(match->string) &&
 			match->string[strlen(match->string)-1] == '\n')
@@ -1905,7 +1905,7 @@ bot_replychat_t *BotLoadReplyChat(char *filename)
 					} //end if
 					StripDoubleQuotes(token.string);
 					if (strlen(namebuffer)) strcat(namebuffer, "\\");
-					strcat(namebuffer, token.string);
+					Q_strcat(namebuffer, sizeof(namebuffer), token.string);
 				} while(PC_CheckTokenString(source, ","));
 				if (!PC_ExpectTokenString(source, ">"))
 				{
@@ -2389,7 +2389,7 @@ void BotConstructChatMessage(bot_chatstate_t *chatstate, char *message, unsigned
 	int i;
 	char srcmessage[MAX_MESSAGE_SIZE];
 
-	strcpy(srcmessage, message);
+	Q_strncpyz(srcmessage, message, sizeof(srcmessage));
 	for (i = 0; i < 10; i++)
 	{
 		if (!BotExpandChatMessage(chatstate->chatmessage, srcmessage, mcontext, match, vcontext, reply))
@@ -2630,7 +2630,7 @@ int BotReplyChat(int chatstate, char *message, int mcontext, int vcontext, char 
 	cs = BotChatStateFromHandle(chatstate);
 	if (!cs) return qfalse;
 	Com_Memset(&match, 0, sizeof(bot_match_t));
-	strcpy(match.string, message);
+	Q_strncpyz(match.string, message, sizeof(match.string));
 	bestpriority = -1;
 	bestchatmessage = NULL;
 	bestrchat = NULL;

codemp/botlib/l_precomp.cpp
@@ -996,14 +996,14 @@ int PC_Directive_include(source_t *source)
 		script = LoadScriptFile(token.string);
 		if (!script)
 		{
-			strcpy(path, source->includepath);
-			strcat(path, token.string);
+			Q_strncpyz(path, source->includepath, sizeof(path));
+			Q_strcat(path, sizeof(path), token.string);
 			script = LoadScriptFile(path);
 		} //end if
 	} //end if
 	else if (token.type == TT_PUNCTUATION && *token.string == '<')
 	{
-		strcpy(path, source->includepath);
+		Q_strncpyz(path, source->includepath, sizeof(path));
 		while(PC_ReadSourceToken(source, &token))
 		{
 			if (token.linescrossed > 0)
@@ -3019,7 +3019,7 @@ void PC_UnreadToken(source_t *source, token_t *token)
 //============================================================================
 void PC_SetIncludePath(source_t *source, char *path)
 {
-	strncpy(source->includepath, path, MAX_PATH);
+	Q_strncpyz(source->includepath, path, MAX_PATH);
 	//add trailing path seperator
 	if (source->includepath[strlen(source->includepath)-1] != '\\' &&
 		source->includepath[strlen(source->includepath)-1] != '/')

codemp/botlib/l_script.cpp
@@ -1383,7 +1383,7 @@ script_t *LoadScriptMemory(char *ptr, int length, char *name)
 	buffer = GetClearedMemory(sizeof(script_t) + length + 1);
 	script = (script_t *) buffer;
 	Com_Memset(script, 0, sizeof(script_t));
-	strcpy(script->filename, name);
+	Q_strncpyz(script->filename, name, sizeof(script->filename));
 	script->buffer = (char *) buffer + sizeof(script_t);
 	script->buffer[length] = 0;
 	script->length = length;

codemp/client/snd_ambient.cpp
@@ -296,10 +296,10 @@ subWaves <directory> <wave1> <wave2> ...
 
 static void AS_GetSubWaves( ambientSet_t &set )
 {
-	char	dirBuffer[512], waveBuffer[256], waveName[1024];
+	char	dirBuffer[512] = {0}, waveBuffer[256] = {0}, waveName[1024] = {0};
 
 	//Get the directory for these sets
-	sscanf( parseBuffer+parsePos, "%s %s", tempBuffer, dirBuffer );	
+	if(sscanf( parseBuffer+parsePos, "%s %s", tempBuffer, dirBuffer ) < 2) return;	
 
 	//Move the pointer past these two strings
 	parsePos += ((strlen(keywordNames[SET_KEYWORD_SUBWAVES])+1) + (strlen(dirBuffer)+1));

codemp/client/snd_mem.cpp
@@ -275,7 +275,7 @@ char *Filename_WithoutExt(const char *psFilename)
 {
 	static char sString[MAX_QPATH];	// !
 
-	strcpy(sString,psFilename);
+	Q_strncpyz(sString, psFilename, sizeof(sString));
 
 	char *p = strrchr(sString,'.');		
 	char *p2= strrchr(sString,'\\');

commit c76bdb3e6dda2b9ccc929a1b58c935873255650f

Fixed display glitch caused by c92207fb1f3d9c56231c71d0853e4609138abe1f

codemp/qcommon/stringed_ingame.cpp
@@ -623,7 +623,7 @@ const char *CStringEdPackage::ParseLine( const char *psLine )
 			{
 				static const char sSeperators[] = " \t";
 				char sFlags[1024]={0};	// 1024 chars should be enough to store 8 flag names
-				Q_strncpyz(sFlags, psLine, sizeof(sFlags));
+				Q_strncpyz(sFlags, psLine, sizeof(sFlags)-1);
 				char *psToken = strtok( sFlags, sSeperators );
 				while( psToken != NULL )
 				{
@@ -673,8 +673,8 @@ const char *CStringEdPackage::ParseLine( const char *psLine )
 					iCharsToCopy = sizeof(sThisLanguage)-1;
 				}
 
-//				strncpy(sThisLanguage, psLine, iCharsToCopy);	// already declared as {0} so no need to zero-cap dest buffer
-				Q_strncpyz(sThisLanguage, psLine, iCharsToCopy);	// yea, whatever
+				strncpy(sThisLanguage, psLine, iCharsToCopy);	// already declared as {0} so no need to zero-cap dest buffer
+//				Q_strncpyz(sThisLanguage, psLine, iCharsToCopy);	// yea, whatever
 
 				psLine += strlen(sThisLanguage);
 				const char *_psSentence = ConvertCRLiterals_Read( InsideQuotes( psLine ) );