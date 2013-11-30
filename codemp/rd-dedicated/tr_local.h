fe6aa40e3d41237fd108370940faa8feccff5ffd
Ensiform <ensiform@gmail.com>
2013-11-30 10:26:58 -0600

    [MP] Remove unused md4 prototypes.

--- codemp/rd-dedicated/tr_local.h
+++ codemp/rd-dedicated/tr_local.h
@@ -1589,8 +1589,6 @@ ANIMATED MODELS
 =============================================================
 */
 
-void R_MakeAnimModel( model_t *model );
-void R_AddAnimSurfaces( trRefEntity_t *ent );
 /*
 Ghoul2 Insert Start
 */
