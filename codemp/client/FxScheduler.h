4aec5b268cb71cccc5babf66e769637a33f77744
Xycaleth <alex@acslo.com>
2014-03-15 08:48:15 +0000

    [MP] Increase max number of scheduled EFX to 2048
    The max number of scheduled EFX prior to this commit was 500, but some
    maps were hitting this limit (such as atlantica). Increasing to 2048
    should be enough for any map...

--- codemp/client/FxScheduler.h
+++ codemp/client/FxScheduler.h
@@ -544,7 +544,7 @@ private:
 	// List of scheduled effects that will need to be created at the correct time.
 	TScheduledEffect	mFxSchedule;
 
-	PoolAllocator<SScheduledEffect, 500> mScheduledEffectsPool;
+	PoolAllocator<SScheduledEffect, 2048> mScheduledEffectsPool;
 
 	// Private function prototypes
 	SEffectTemplate *GetNewEffectTemplate( int *id, const char *file );
