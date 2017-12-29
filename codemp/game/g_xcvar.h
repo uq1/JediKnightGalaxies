9ae48e2eea7e1f0438530d10727d24c594268cff
DarthFutuza <darthfutuza@hotmail.com>
2017-12-29 12:35:54 -0700

    Change default bounty to 125

--- codemp/game/g_xcvar.h
+++ codemp/game/g_xcvar.h
@@ -169,7 +169,7 @@ XCVAR_DEF( timelimit,					"45",			NULL,				CVAR_SERVERINFO|CVAR_ARCHIVE|CVAR_NOR
 // Jedi Knight Galaxies
 XCVAR_DEF( jkg_shop_replenish_time,		"300",			NULL,				CVAR_ARCHIVE,									true )
 XCVAR_DEF( jkg_startingCredits,			"500",			NULL,				CVAR_ARCHIVE|CVAR_LATCH|CVAR_SERVERINFO,		false )
-XCVAR_DEF( jkg_bounty,					"75",			NULL,				CVAR_ARCHIVE,									true )
+XCVAR_DEF( jkg_bounty,					"125",			NULL,				CVAR_ARCHIVE,									true )
 XCVAR_DEF( jkg_killsPerBounty,			"2",			NULL,				CVAR_ARCHIVE,									true )
 XCVAR_DEF( jkg_creditsPerKill,			"150",			NULL,				CVAR_ARCHIVE,									true )
 XCVAR_DEF( jkg_minAssistAwardRatio,		"20",			NULL,				CVAR_ARCHIVE,									true )	//value is % out of 100, Max allowed value is 50
