8ec4380e7648f20b3c48a2c39e8a00d33c2d3ddc
DarthFutuza <darthfutuza@hotmail.com>
2018-11-12 13:09:58 -0700

    Adding test spice item, with example script for consumables that do damage

--- JKGalaxies/glua/resources/items/item_spice.lua
+++ JKGalaxies/glua/resources/items/item_spice.lua
@@ -4,16 +4,17 @@ local SPICE_SMALL_AMOUNT = 10
 local SPICE_LARGE_AMOUNT = 25
 
 function GiveSpiceSmall(ply, quantity)
-	if ply.Health - SPICE_SMALL_AMOUNT < 1 then
-		ply.Health = 0
-	else
-		ply.Health = ply.Health - SPICE_SMALL_AMOUNT
-	end
+	ply:Damage(ply:GetEntity(ply), ply:GetEntity(ply), ply:GetOrigin(ply), ply:GetOrigin(ply), SPICE_SMALL_AMOUNT, 0, 3)
+	--if ply.Health - SPICE_SMALL_AMOUNT < 1 then
+		--ply.Health = 0
+	--else
+		--ply.Health = ply.Health - SPICE_SMALL_AMOUNT
+	--end
 
 	ply.Entity:PlaySound(1, "sound/items/use_bacta.wav")
 end
 
-function GiveAntidoteLarge(ply, quantity)
+function GiveSpiceLarge(ply, quantity)
 	if ply.Health - SPICE_LARGE_AMOUNT < 1 then
 		ply.Health = 0
 	else
